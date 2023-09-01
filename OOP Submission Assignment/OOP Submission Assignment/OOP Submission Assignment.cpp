08e66ba54c312a287a070f0ece66d3976f35effe
LonelyJenkins <124760225+LonelyJenkins@users.noreply.github.com>
2023-09-01 00:39:15 -0500

    Created Submission Assignment
    Includes other small files

---
+++ OOP Submission Assignment/OOP Submission Assignment/OOP Submission Assignment.cpp
@@ -0,0 +1,57 @@
+// OOP Submission Assignment.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class dog
+{
+public:
+    string breed;
+    string color;
+    string name;
+    int weight;
+    int height;
+
+    void Shake()
+    {
+        cout << name + " shakes their body gracefully";
+    }
+
+    void Sit()
+    {
+        cout << name + " sits down gracedully, like a good " + breed;
+    }
+
+    void LayDown()
+    {
+        cout << name + " lays down with the soft elegance of a dove landing into the hands of a beloved caretaker. Awesome!";
+    }
+};
+
+
+int main()
+{
+    dog Hound;
+    Hound.breed = "Hound";
+    Hound.color = "brown";
+    Hound.name = "Harold";
+    Hound.weight = 60;
+    Hound.height = 2;
+
+    Hound.Shake();
+    Hound.LayDown();
+    Hound.Sit();
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
